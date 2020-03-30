aecfda334e513e0d3305b6787a3dc09ca423d1a5
Jim Verheijde <jim.verheijde@hotmail.com>
2020-03-30 17:46:04 +0200

    Change to introspective graphql

--- main.py
+++ main.py
@@ -1,22 +1,64 @@
-from gql import gql, Client
-from graphql import build_ast_schema, parse
+import os
+
 from dotenv import load_dotenv
+from gql import Client, gql
+from gql.transport.requests import RequestsHTTPTransport
 
-load_dotenv()
-
-with open('path/to/schema.graphql') as source:
-    document = parse(source.read())
-
-schema = build_ast_schema(document)
-
-client = Client(schema=schema, headers={'Authorization': os.getenv("GITHUB_ACCESS_TOKEN")})
-query = gql('''
-    {
-      hello
-    }
-''')
-
-client.execute(query)
+# Change this variable for different components
+COMPONENT = 'fft'
 
 if __name__ == "__main__":
+    load_dotenv()
 
+    sample_transport = RequestsHTTPTransport(
+        url='https://api.github.com/graphql',
+        use_json=True,
+        headers={
+            'Authorization': 'bearer ' + os.getenv("GITHUB_ACCESS_TOKEN"),
+        },
+        verify=False
+    )
+
+    client = Client(
+        transport=sample_transport,
+        fetch_schema_from_transport=True,
+    )
+    query = gql('''
+{
+  repository(owner: "numpy", name: "numpy") {
+    pullRequests(states: MERGED, last: 100, labels: "component: numpy.{component}") {
+      edges {
+        node {
+          title
+          mergedBy {
+            login
+          }
+          comments(first: 100) {
+            edges {
+              node {
+                author {
+                  login
+                }
+              }
+            }
+          }
+          participants(first: 100) {
+            totalCount
+            edges {
+              node {
+                login
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
+    '''.format(component=COMPONENT))
+
+    res = client.execute(query)
+
+    print(res)
